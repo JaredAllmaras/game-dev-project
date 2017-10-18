638d30729446c7991cd98f44710553cce08f8c25
MeeraWakim <meera.wakim925@gmail.com>
2017-10-18 11:42:15 -0500

    ADDED A START SCREEN :)

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var game = new Phaser.Game(1131,621, Phaser.AUTO);
+//creating the states
 game.state.add('state0', demo.state0);
 game.state.add('state1', demo.state1);
-game.state.start('state1');
+
+//sending user to initial state
+game.state.start('state0');
