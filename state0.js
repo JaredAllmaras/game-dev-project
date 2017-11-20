6bb3c4c0472866a7ed3af5cac9973850b1d7c6df
MeeraWakim <meera.wakim925@gmail.com>
2017-11-20 13:38:06 -0600

    made edits to start screen

--- state0.js
+++ state0.js
@@ -4,15 +4,25 @@ var text;
 var counter = 0;
 demo.state0 = function(){};
 
+
 demo.state0.prototype = {
     
     preload: function(){
          game.load.image('logo', 'assets/death_decimation logo.png');  
-        
+         game.load.spritesheet('button', 'assets/sprites/button_sprite_sheet.png', 193, 71);
+        game.load.image('background','assets/starfield.jpg');
     },
     
+//var button;
+//var background;
+    
     create: function(){
-        //state0.body.style.backgroundColor = "red";
+        game.stage.backgroundColor = '#000000';
+        //background = game.add.tileSprite(0, 0, 1280,800, 'background');
+        var image = game.add.sprite(game.world.centerX - 95, 400, 'button');
+
+
+    
         //loading in the logo
         var image = game.add.sprite(game.world.centerX, 225, 'logo');
         image.width = 600;
@@ -31,10 +41,15 @@ demo.state0.prototype = {
          
         if(game.input.activePointer.isDown){
         game.state.start('state1');
-        
-        }
-      
-    }
     
+        
+    }
+   /* whenbuttonispressed: function(){
+        game.state.start('state1');
+    }*/
+
+
+    
+}
 };
 
