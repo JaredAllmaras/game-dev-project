2eff20892fe2fa0312cfabda717b2f4205b3152a
MeeraWakim <meera.wakim925@gmail.com>
2017-11-27 13:46:52 -0600

    added "click anywhere to start"

--- state0.js
+++ state0.js
@@ -3,6 +3,7 @@ var demo = {};
 var text1;
 var text2;
 var text3;
+var startgame;
 var counter = 0;
 demo.state0 = function(){};
 
@@ -26,14 +27,21 @@ demo.state0.prototype = {
         image.height = 600;
         image.anchor.set(0.5);
 
-        //  Enables all kind of input actions on this image (click, etc)
+        //enables input actions
         image.inputEnabled = true;
         
+        //adding all text
         text1 = game.add.text(50, 330, 'DIRECTIONS:', { font: '50px Arial', fill: '#ffffff' } );
-        
+
         text2 = game.add.text(50, 410, 'Use wasd controls to move.\nUse your cursor to shoot zombies.\nHealth and home boosters will\nincrease health\nProtect yourself and your home\nat all costs.', { font: '25px Arial', fill: '#ffffff' });
         
         text3 = game.add.text(700, 330, 'YOUR HUNTER:', { font: '50px Arial', fill: '#ffffff' } );
+        
+        startgame = game.add.text(game.world.centerX,700,'CLICK ANYWHERE TO START', { font: '50px Arial', fill: '#ffffff' });
+        startgame.anchor.set(0.5);
+        
+        
+        //adding hunter image
         var hunterimg = game.add.sprite(810,430,'hunter');
         hunterimg.width = 200;
         hunterimg.height = 200;
